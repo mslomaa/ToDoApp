dc90b132165ee3e72ae8aa3d95d7898a1028c367
Mateusz Slomkowski <slomkowskimateusz@gmail.com>
2020-05-07 16:59:56 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const removeTask = (e) => {
+    e.target.remove();
+
+}
+
+document.querySelectorAll('li').forEach(item => item.addEventListener('click', removeTask))
+
+const arr = [];
+
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
+const liElements = document.querySelectorAll('li');
+
+const searchTask = (e) => {
+    const searchText = e.target.value.toLowerCase();
+    let tasks = [...liElements];
+    tasks = tasks.filter(li => li.textContent.toLowerCase().includes(searchText));
+    ul.textContent = '';
+    tasks.forEach(li => ul.appendChild(li))
+    
+}
+
+input.addEventListener('input', searchTask)
